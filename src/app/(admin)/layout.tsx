774a340c2cd67c361af85cd71e53440a4d4ebb84
Killian Gemoets <kilgem@live.fr>
2024-07-10 23:05:46 +0200

    fix: build

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,4 +1,4 @@
-import bgBackoffice from "@/assets/images/background/bg-backoffice.webp";
+import bgBackoffice from "@/assets/images/backgrounds/bg-backoffice.jpg";
 
 export default async function AdminLayout({
   children,
