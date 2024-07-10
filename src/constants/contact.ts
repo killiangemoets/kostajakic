c014f64e0e4d816fa09af8065885cb45168142ae
Killian Gemoets <kilgem@live.fr>
2024-07-10 12:15:30 +0200

    fix: build

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,6 +1,6 @@
 export const CONTACT_EMAIL = "kosta.jakic@gmail.com";
 export const CONTACT_PHONE_NUMBER = "+32 474 93 71 11";
 
-export const INSTAGRAM_URL = "https://www.instagram.com/kosta.jakic/";
-export const FACEBOOK_URL = "https://www.facebook.com/kosta.jakic";
+export const INSTAGRAM_URL = "https://www.instagram.com/kostajakicpianist/";
+export const FACEBOOK_URL = "https://www.facebook.com/jakickosta";
 export const YOUTUBE_URL = "https://www.youtube.com/channel/kojakic";
