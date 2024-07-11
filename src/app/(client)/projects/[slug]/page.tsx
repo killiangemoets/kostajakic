9b8e1ec56e617c1843c868ba3eea1d91e671fc2c
Killian Gemoets <kilgem@live.fr>
2024-07-11 12:27:28 +0200

    feat: project page responsiveness

--- src/app/(client)/projects/[slug]/page.tsx
+++ src/app/(client)/projects/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { Typography } from "@/components/typography";
+import { Button } from "@/components/ui/button";
 import { PROJECTS } from "@/constants/projects";
+import { ArrowBigLeft } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -28,18 +30,30 @@ export default async function Projects({ params }: Readonly<{ params: { slug: st
   const project = getProject(params.slug);
   if (!project) return <Typography.body>Project not found</Typography.body>;
   return (
-    <div className="w-full sm:w-[50%] sm:min-w-[500px] space-y-8">
-      <Typography.h3 className="border-b font-bold normal-case">{project.title}</Typography.h3>
-      <div className="flex gap-4">
-        <Typography.body className="font-medium">
-          {project.description?.split("\n").map((line, i) => (
-            <React.Fragment key={i}>
-              {line}
-              <br />
-            </React.Fragment>
-          ))}
-        </Typography.body>
-        <Image className="w-[25%] h-fit object-cover object-center" src={project.image} alt={project.title} priority />
+    <div className="w-full sm:w-[56%] sm:min-w-[500px] space-y-0 flex flex-col sm:flex-row gap-4">
+      <Button href="/projects" variant="outline" size="icon" className="h-fit w-fit rounded-full border-[1.5px] px-1 py-1">
+        <ArrowBigLeft className="h-6 w-6 stroke-[1.5px]" />
+      </Button>
+      <div className="space-y-8">
+        <Typography.h3 className="font-bold border-b normal-case">{project.title}</Typography.h3>
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-[68%_32%] lg:gap-0 w-full">
+          <Typography.body className="font-medium lg:mr-4">
+            {project.description?.split("\n").map((line, i) => (
+              <React.Fragment key={i}>
+                {line}
+                <br />
+              </React.Fragment>
+            ))}
+          </Typography.body>
+          <div className="row-start-1 lg:row-auto w-full flex justify-center">
+            <Image
+              className="max-h-[400px] lg:max-h-none w-fit h-fit object-cover object-center"
+              src={project.image}
+              alt={project.title}
+              priority
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
