459e826cd34b8b38092a091b46887159b176ccfd
Killian Gemoets <kilgem@live.fr>
2024-07-20 20:18:38 +0200

    fix: backdrop filter on phone

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { DesktopNavbar } from "./desktop";
-import { PhonseNavbar } from "./phone";
+import { PhoneNavbar } from "./phone";
 import { useScreenWidth } from "@/hooks/useScreenWidth";
 
 export const Navbar = () => {
   const { widthSize } = useScreenWidth();
 
-  return <>{["xxs", "xs", "sm", "md"].includes(widthSize) ? <PhonseNavbar /> : <DesktopNavbar />}</>;
+  return <>{["xxs", "xs", "sm", "md"].includes(widthSize) ? <PhoneNavbar /> : <DesktopNavbar />}</>;
 };
