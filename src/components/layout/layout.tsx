e35727c16c8722c29a0e9ece101236cca782665f
Killian Gemoets <kilgem@live.fr>
2024-07-21 13:02:05 +0200

    fix: body classes

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -13,7 +13,7 @@ export const Layout = {
     ...props
   }: { backgroundImg?: StaticImageData; bgFilter?: boolean; backgroundPosition?: string } & React.ComponentPropsWithoutRef<"div">) => (
     <div className="min-h-dvh px-8 pt-8 pb-16 overflow-hidden flex flex-col gap-16 relative" {...props}>
-      {/* {!!backgroundImg && (
+      {!!backgroundImg && (
         <div className="fixed top-0 left-0 w-full h-full">
           <Image
             priority={true}
@@ -28,7 +28,7 @@ export const Layout = {
           />
           {bgFilter && <div className="bg-primary-700/60 md:bg-primary-700/20 lg:bg-primary-700/0 absolute top-0 left-0 w-full h-full" />}
         </div>
-      )} */}
+      )}
       <Navbar />
       <main className={cn("relative px-20 flex-1", className)}>{children}</main>
     </div>
