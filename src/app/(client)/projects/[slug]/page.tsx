d2d0ff21d9daa3efaddcb7c6b6526f953120bd57
Killian Gemoets <kilgem@live.fr>
2024-08-06 14:57:29 +0200

    feat: about + projects

--- src/app/(client)/projects/[slug]/page.tsx
+++ src/app/(client)/projects/[slug]/page.tsx
@@ -30,14 +30,14 @@ export default async function Projects({ params }: Readonly<{ params: { slug: st
   const project = getProject(params.slug);
   if (!project) return <Typography.body>Project not found</Typography.body>;
   return (
-    <div className="w-full sm:w-[56%] sm:min-w-[500px] space-y-0 flex flex-col sm:flex-row gap-4">
+    <div className="w-full sm:w-[66%] sm:min-w-[500px] space-y-0 flex flex-col sm:flex-row gap-4">
       <Button href="/projects" variant="outline" size="icon" className="h-fit w-fit rounded-full border-[1.5px] px-1 py-1">
         <ArrowBigLeft className="h-6 w-6 stroke-[1.5px]" />
         <span className="sr-only">Back</span>
       </Button>
       <div className="space-y-8">
         <Typography.h2 className="font-bold border-b normal-case">{project.title}</Typography.h2>
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-[68%_32%] lg:gap-0 w-full">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-[68%_32%] lg:gap-1 w-full">
           <Typography.body className="font-medium lg:mr-4">
             {project.description?.split("\n").map((line, i) => (
               <React.Fragment key={i}>
--- src/content/projects.ts
+++ src/content/projects.ts
@@ -1,52 +1,20 @@
 import project1 from "@/assets/images/projects/project_1.webp";
 import project2 from "@/assets/images/projects/project_2.webp";
-import project3 from "@/assets/images/projects/project_3.webp";
-import project4 from "@/assets/images/projects/project_4.webp";
-import project5 from "@/assets/images/projects/project_5.webp";
-import project6 from "@/assets/images/projects/project_6.webp";
 import type { Project } from "@/types/projects";
 
 export const PROJECTS: Project[] = [
   {
-    title: "Moris Rackmanovsky Project",
-    slug: "moris-rackmanovsky",
+    title: "Collaboration with Simon Gronowski: Life without music is impossible",
+    slug: "life-without-music-is-impossible",
     image: project1,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur officiis provident maiores repellendus id iusto doloremque accusamus animi culpa est, quibusdam natus adipisci saepe rem amet perspiciatis fugit eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem ad numquam possimus quasi culpa sit non libero quo maiores eius qui, cumque harum fugit dolor labore voluptas repellat velit.",
+      'A creative indisciplinary and intergenerational project on the topic of memory, transmission and peace, to oppose the rise of the extreme right with jazz and classical music concerts, other forms of musical expression, exhibition, conferences, screening of 2 documentaries with still living participants of the Second World War and school sessions. The platform was conceived as a continuation of the inter-generational encounter between Simon Gronowski, doctor of law and jazz music lover (92), who jumped from the train at the persuasion of his mother to save himself from Auschwitz, unlike the rest of his family, and Kosta Jakić(27), a pianist of Yugoslav origin from Belgium who studies the role of music in extreme conditions (in prisons, camps, etc.). Ivan Put, a Belgian videographer and photographer, made a film in cooperation with the platform for aesthetic education of all PhDs In One Night, Laboratory of Radical Peace and Orangeries de Bierbais entitled "Life without music is impossible" according to a quote by Simon Gronowski, who is a lifelong memory of a sister , young pianist who died in Auschwitz, and in memory of her he continued to love and study jazz music as a piano amateur. Film englobe series of concerts that have already been held in the Dossin camp and Breendonk in Belgium by Simon (jazz) and Kosta ( classical music) and conversations between 2 pianists, amateur and professional one. The projection of an intergenerational film is planned for 2025 as a place of conversation with all interested participants on the topic of the transmission of memories and the role of music in our lives (music has been excluded from primary education for about 20 years from school programs in Europe).',
   },
   {
-    title: "Mozart l'Opera Rock Project",
-    slug: "mozart-opera-rock",
+    title: "Belgian Doctor Symphonny Orchestra",
+    slug: "belgian-doctor-symphonny-orchestra",
     image: project2,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur officiis provident maiores repellendus id iusto doloremque accusamus animi culpa est, quibusdam natus adipisci saepe rem amet perspiciatis fugit eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem ad numquam possimus quasi culpa sit non libero quo maiores eius qui, cumque harum fugit dolor labore voluptas repellat velit.",
-  },
-  {
-    title: "Les Misérables Project",
-    slug: "les-miserables",
-    image: project3,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur officiis provident maiores repellendus id iusto doloremque accusamus animi culpa est, quibusdam natus adipisci saepe rem amet perspiciatis fugit eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem ad numquam possimus quasi culpa sit non libero quo maiores eius qui, cumque harum fugit dolor labore voluptas repellat velit.",
-  },
-  {
-    title: "The Phantom of the Opera",
-    slug: "phantom-opera",
-    image: project4,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur officiis provident maiores repellendus id iusto doloremque accusamus animi culpa est, quibusdam natus adipisci saepe rem amet perspiciatis fugit eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem ad numquam possimus quasi culpa sit non libero quo maiores eius qui, cumque harum fugit dolor labore voluptas repellat velit.",
-  },
-  {
-    title: "The Beatles Project",
-    slug: "beatles",
-    image: project5,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur officiis provident maiores repellendus id iusto doloremque accusamus animi culpa est, quibusdam natus adipisci saepe rem amet perspiciatis fugit eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem ad numquam possimus quasi culpa sit non libero quo maiores eius qui, cumque harum fugit dolor labore voluptas repellat velit.",
-  },
-  {
-    title: "The Rolling Stones Project",
-    slug: "rolling-stones",
-    image: project6,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur officiis provident maiores repellendus id iusto doloremque accusamus animi culpa est, quibusdam natus adipisci saepe rem amet perspiciatis fugit eum. Lorem ipsum dolor sit amet consectetur adipisicing",
+      'The BDSO was born from the desire and initiative of Lina Berada and Kosta Jakic to combine two fields: classical music and medicine. One heals the body, and the other heals the soul. Theodor Billroth, one of the founding fathers of modern abdominal surgery and a close friend of Brahms, once said:\n "It is one of the superficialities of our time to see science and art as opposites; imagination is the mother of both."\n\nThis philosophy inspired us to organise our first concert at the prestigious Institut Bordet, an internationally renowned oncology centre, which welcomed us with open arms.\n This concert is a unique opportunity to raise public awareness of the importance of art and medicine as complementary fields.\n\nBy showcasing doctors and medical students who also cultivate their musical talents, this event helps break stereotypes and demonstrates that healthcare professionals are multifaceted individuals. It reinforces the idea that artistic creativity and medical compassion are two essential qualities that can be nurtured in parallel for overall well-being.',
   },
 ] as const;
