e4da5106896bb648d0e196c339f37fa87c66fe66
Killian Gemoets <kilgem@live.fr>
2024-08-06 17:06:36 +0200

    fix: logo

--- src/components/layout/navbar/desktop.tsx
+++ src/components/layout/navbar/desktop.tsx
@@ -1,20 +1,19 @@
-// import kostaJakicLogo from "@/assets/icons/kj.png";
+import kostaJakicLogo from "@/assets/icons/kj.png";
 import { Button } from "@/components/ui/button";
 import { NAVBAR_ITEMS } from "@/constants/navigation";
-
-// import Image from "next/image";
+import Image from "next/image";
 
 export const DesktopNavbar = () => {
   return (
     <header className="flex gap-6 xl:gap-8 z-10">
-      <Button size="lg" className="mt-0" variant="ghost" href="/">
+      {/* <Button size="lg" className="mt-0" variant="ghost" href="/">
         Kosta.
         <span className="sr-only">Home</span>
-      </Button>
-      {/* <Button size="lg" className="mt-0" variant="ghost" href="/">
+      </Button> */}
+      <Button size="lg" className="mt-0" variant="ghost" href="/">
         <Image className="w-14" src={kostaJakicLogo} alt="kosta jakic logo" />
         <span className="sr-only">Home</span>
-      </Button> */}
+      </Button>
 
       <nav>
         <ul className="flex gap-6 xl:gap-8">
